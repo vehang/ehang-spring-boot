782eeeb9bc84b49eca01d8c4e49cd444b6970fd6
ehang <397729842@qq.com>
2022-08-18 14:41:14 +0800

    添加docker的项目

---
+++ spring-boot-012-tools-docker/src/main/java/com/ehang/tools/docker/SpringBoot012ToolsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.ehang.tools.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot012ToolsDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot012ToolsDockerApplication.class, args);
+    }
+
+}
---
+++ spring-boot-012-tools-docker/src/test/java/com/ehang/tools/docker/SpringBoot012ToolsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehang.tools.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot012ToolsDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
