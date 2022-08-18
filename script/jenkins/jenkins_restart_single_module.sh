b9bd2b2811da3e1d3ba8ae9b1c6392a7d5c3c896
ehang <397729842@qq.com>
2022-08-18 19:13:38 +0800

    调整单模块重启的脚本

--- script/jenkins/jenkins_restart_single_module.sh
+++ script/jenkins/jenkins_restart_single_module.sh
@@ -9,7 +9,5 @@ ps -ef | grep $JAR_FILE | grep -v grep | awk '{print $2}' | xargs kill -9
 
 java -jar $JAR_PATH/$JAR_FILE > $JAR_PATH/out.log &
 if [ $? = 0 ];then
-        #sleep 30
-        #tail -n 50 out.log
         echo  $JAR_FILE" restart success!!! process id:"`ps -ef | grep $JAR_FILE | grep -v grep | awk '{print $2}'`
 fi
