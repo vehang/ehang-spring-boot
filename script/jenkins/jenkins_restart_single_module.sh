66a665ac001125cbc6dc1b99a87a971c9e00c1f5
ehang <397729842@qq.com>
2022-08-19 14:34:55 +0800

    调整脚本

--- script/jenkins/jenkins_restart_single_module.sh
+++ script/jenkins/jenkins_restart_single_module.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # JDK的环境变量
 export JAVA_HOME=/usr/local/jdk-11.0.14
 export PATH=$JAVA_HOME/bin:$PATH
@@ -8,7 +10,7 @@ JAR_FILE=spring-boot-001-hello-world-0.0.1-SNAPSHOT.jar
 ps -ef | grep $JAR_FILE | grep -v grep | awk '{print $2}' | xargs kill -9
 
 #BUILD_ID=dontKillMe
-java -jar $JAR_PATH/$JAR_FILE > $JAR_PATH/out.log 2>&1 &
+nohup java -jar $JAR_PATH/$JAR_FILE > $JAR_PATH/out.log 2>&1 &
 if [ $? = 0 ];then
         echo  $JAR_FILE" restart success!!! process id:"`ps -ef | grep $JAR_FILE | grep -v grep | awk '{print $2}'`
 fi
