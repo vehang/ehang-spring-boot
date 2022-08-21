7f3c5bd1086a52d2a5b9da106044b0cde67a84fd
ehang <397729842@qq.com>
2022-08-21 14:02:52 +0800

    调整脚本

--- spring-boot-001-hello-world/docker/docker-image-build.sh
+++ spring-boot-001-hello-world/docker/docker-image-build.sh
@@ -1,9 +1,11 @@
 mkdir ./tmp
 
-cp ./ehang-sping-boot-hello-world/docker/* ./tmp
-cp ./ehang-sping-boot-hello-world/target/*.jar ./tmp
+cp ./spring-boot-001-hello-world/docker/* ./tmp
+cp ./spring-boot-001-hello-world/target/*.jar ./tmp
+cd ./tmp
 
 docker build -t registry.cn-guangzhou.aliyuncs.com/ehang_jenkins/ehang-sping-boot-hello-world:latest ./tmp/.
 docker push registry.cn-guangzhou.aliyuncs.com/ehang_jenkins/ehang-sping-boot-hello-world:latest
 
+cd ..
 #rm -rf ./tmp
